7fa3c484f554ddf24fa4adf57f74aacf9c5b862b
Gosia <majg.tychy@gmail.com>
2021-02-24 21:15:33 +0100

    Rozwijanie aplikacji

--- projects/dobre-mysli/src/main/java/projects/goodthoughts/GoodThoughtsApp.java
+++ projects/dobre-mysli/src/main/java/projects/goodthoughts/GoodThoughtsApp.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import projects.goodthoughts.model.Quote;
 
 import java.time.LocalDateTime;
+import java.util.Scanner;
+import java.util.Set;
 
 public class GoodThoughtsApp {
 
@@ -20,5 +22,71 @@ public class GoodThoughtsApp {
         logger.debug("Test cytatu 1:" + quote1);
         logger.debug("Test cytatu 2: {}", quote2);
         // druga wersja {} tożsama z pierwszą
+        logger.debug(String.format("Oba cytaty, 1: %s,2: %s ", quote1,quote2));
+
+        GoodThoughtsApp app = new GoodThoughtsApp();
+        app.run();
+    }
+    
+    public void run(Object... args){
+        logger.info("Rozpoczecie pracy z aplikacją");
+        showWelcomeMessage();
+        while(true){
+            showMenu();
+            String userInput = getUserInput();
+            boolean isValidOption = validateOption(userInput);
+            if(isValidOption){
+                //String option = userInput;
+                executeOption(userInput);
+                boolean isExitOption = checkIsExitOption(userInput);
+                if(isExitOption){
+                    break;
+                }
+            }else{
+                showInvalidOptionMessage(userInput);
+            }
+        }
+        showGoodbyeMessage();
+        logger.info("Zakończono pracę z aplikacją");
+    }
+
+    private void showGoodbyeMessage() {
+        System.out.println("Bye, bye");
+    }
+
+    private void showInvalidOptionMessage(String userInput) {
+        System.out.println("Wybrales nieprawidlowa opcje");
+    }
+
+    private boolean checkIsExitOption(String userInput) {
+        return "0".equals(userInput);
+    }
+
+    private void executeOption(String option) {
+
+    }
+
+    private boolean validateOption(String userInput) {
+        Set <String> validOption = Set.of("0", "1","2","3");
+        return validOption.contains(userInput);
+    }
+
+    private String getUserInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Twoj wybor: ");
+        String userInput = sc.nextLine();
+        return userInput.trim();
+    }
+
+    private void showMenu() {
+        System.out.println("Wybierz opcje z listy ponizej: ");
+        System.out.println("\t1. Pobierz domyslny cytat");
+        System.out.println("\t2. Dodaj wlasny cytat");
+        System.out.println("\t3. Wyswietl zapisane cytaty");
+        System.out.println("\t0. Zakoncz");
+    }
+
+    private void showWelcomeMessage() {
+        System.out.println("Witaj w aplikacji(" + LocalDateTime.now() + ")");
     }
 }
