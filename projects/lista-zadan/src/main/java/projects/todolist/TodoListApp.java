2bcb21e93f44d67e021fbbaab7f0d7289d993e93
Gosia82 <majg.tychy@gmail.com>
2021-02-28 19:42:55 +0100

    create package projects.todolist and class TodoList in this package.

---
+++ projects/lista-zadan/src/main/java/projects/todolist/TodoListApp.java
@@ -0,0 +1,15 @@
+package projects.todolist;
+
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+
+public class TodoListApp {
+    private static final Logger logger = LoggerFactory.getLogger(TodoListApp.class);
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
+
